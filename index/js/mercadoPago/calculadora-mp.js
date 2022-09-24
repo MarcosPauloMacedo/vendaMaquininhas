f0fbbdf8e553ddb64bc749f6617e5ecb0baa2446
marcospaulo <marcospaulomacedo@gmail.com>
2022-09-24 16:50:51 -0300

    primeiras funcionalidades das calculadoras de taxas

---
+++ index/js/mercadoPago/calculadora-mp.js
@@ -0,0 +1,85 @@
+class Simulacao{
+    constructor(){
+        let $ = document.querySelector.bind(document)
+        this.valor = $('#mp-valor') //digitar valor da venda
+        this.venda = $('#mp-valorVenda') //Se vender R$ 150, você recebe:
+        this.debito = $('#mp-precoDebito')
+        this.credito = $('#mp-precoCredito')
+        this.precoParcelado = $('#mp-precoParcelado')
+        this.pix = $('#mp-precoPix')
+        this.mpSelect = $('#mp-select')
+        this.mpParcelas = $('#mp-x-parcelas')
+    }
+
+    simular(){
+        let valor = this.valor
+        valor.addEventListener('keyup',()=>{
+            if(valor.value == 0){
+                return this.venda.innerText = 'Se vender R$ 150, você recebe:',
+                this.debito.textContent = 'R$ 147.01',
+                this.credito.textContent = 'R$ 142.89',
+                this.precoParcelado.textContent = 'R$ 142.03',
+                this.pix.textContent = 'R$ 150.00'
+            }
+
+            else{
+                this.venda.
+                innerText = `Se vender R$ ${valor.value},00, você recebe:`
+
+                let taxas = []
+
+                let tarifas = [0.0199,0.0474,0.0531,0]
+                tarifas.forEach((n,i)=>{ 
+                    taxas.push((valor.value - (valor.value * tarifas[i])).toFixed(2))
+                })
+
+                this.debito
+                .textContent = `R$ ${taxas[0]}` 
+                
+                this.credito
+                .textContent = `R$ ${taxas[1]}`
+        
+                this.precoParcelado
+                .textContent = `R$ ${taxas[2]}`
+
+                this.pix
+                .textContent = `R$ ${taxas[3]}`
+            }
+        })
+    }
+
+    mostrar(){
+        this.mpParcelas.addEventListener('change',(event)=>{
+            let taxas = [0.0479,0.0597,0.0733]
+            let tarifas = []
+
+            if(this.valor.value == 0){
+                return this.venda.innerText = 'Se vender R$ 150, você recebe:',
+                this.debito.textContent = 'R$ 147.01',
+                this.credito.textContent = 'R$ 142.89',
+                this.precoParcelado.textContent = 'R$ 142.03',
+                this.pix.textContent = 'R$ 150.00'
+            }
+
+            taxas.forEach((n,i)=>{
+                tarifas.push(this.valor.value - (this.valor.value * taxas[i]))
+            })
+           
+            if(event.target.value == '2x'){
+             this.precoParcelado.textContent = `R$ ${tarifas[0]}`
+            } 
+
+            else if( event.target.value == '3x'){
+             this.precoParcelado.textContent = `R$ ${tarifas[1]}`
+            }
+
+            else{
+             this.precoParcelado.textContent = `R$ ${tarifas[2]}`
+            }
+         })
+    }
+}
+
+const simulacao = new Simulacao()
+simulacao.simular()
+simulacao.mostrar()
---
+++ index/js/pagSeguro/simulacao.js
@@ -0,0 +1,48 @@
+class Calculadora_PagS{
+    constructor(){
+        let pegar = document.querySelector.bind(document)
+
+        this.valor = pegar('#pagSeg-valor')
+        this.parcelas = pegar('#pagSeg-parcelas')
+        this.pagarHora = pegar('#pagSeg-hora')
+        this.pagar14 = pegar('#pagSeg-14')
+        this.pagar30 = pegar('#pagSeg-30')
+    }
+
+    reset(){
+        this.valor.addEventListener('keyup',()=>{
+            this.pagarHora.innerText = 'R$ 0.00'
+            this.pagar14.innerText = 'R$ 0.00'
+            this.pagar30.innerText = 'R$ 0.00'
+            this.parcelas.value = null
+        })
+    }
+
+    simular(){
+        let parcelas = this.parcelas
+
+        let debito = [0.0290,0.0234,0.0654]
+        let credito = [0.0554,0.0185,0.0585]
+        let parcela2x = [0.0912,0.0412,0.0121]
+
+        let pag = ['debito','credito','2x']
+        let tiposPagamento = [debito,credito,parcela2x]
+
+        pag.forEach((n,i)=>{
+            parcelas.addEventListener('change',(event)=>{
+                if(event.target.value == pag[i]){
+                    let valor = parseInt(this.valor.value)
+                    
+                    this.pagarHora.innerText = (valor - (valor * tiposPagamento[i][i])).toFixed(2)
+                    this.pagar14.innerText = (valor - (valor * tiposPagamento[i][1])).toFixed(2)
+                    this.pagar30.innerText = (valor - (valor * tiposPagamento[i][2])).toFixed(2) 
+                }
+            })
+        })
+    }
+}
+
+const pagSeguroSimular = new Calculadora_PagS()
+
+pagSeguroSimular.reset()
+pagSeguroSimular.simular()
